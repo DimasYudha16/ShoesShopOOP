9afbbe6bd7be0e47a306c137a4e5e18a1bc5326f
Dimas Yudha Adi Pascayana <paskayanad@gmail.com>
2023-01-14 00:17:01 +0700

    tinggal nampilin table dan fxml

--- ListSepatu.java
+++ ListSepatu.java
@@ -1,13 +1,26 @@
-public class ListSepatu {
-    Private static arraylist<Sepatu> listSepatu = new arraylist<Sepatu>(); //arraylist untuk menyimpan data sepatu
+import java.util.ArrayList;
 
-    public static void tambahSepatu(string id, string namaSepatu, string ukuranSepatu, string warnaSepatu, int hargaSepatu, int jumlahSepatu) {
-        Sepatu sepatu = new Sepatu(id, namaSepatu, ukuranSepatu, warnaSepatu, hargaSepatu, jumlahSepatu);
+public class ListSepatu {
+    private static ArrayList<Sepatu> listSepatu = new ArrayList<Sepatu>(); //arraylist untuk menyimpan data sepatu
+
+    public static void tambahSepatu(String dataNamaSepatu, String dataUkuranSepatu, String dataWarnaSepatu, String dataHargaSepatu, String dataJumlahSepatu) {
+        Sepatu sepatu = new Sepatu(dataNamaSepatu, dataUkuranSepatu, dataWarnaSepatu, dataHargaSepatu, dataJumlahSepatu);
         listSepatu.add(sepatu);
     }
 
-    public arrayList<Sepatu> getListSepatu() {
+    public ArrayList<Sepatu> getListSepatu() {
         return listSepatu;
     } 
+
+    //display arraylist
+    public static void displayListSepatu() {
+        for (Sepatu sepatu : listSepatu) {
+            System.out.println("Nama Sepatu: " + sepatu.getNamaSepatu());
+            System.out.println("Ukuran Sepatu: " + sepatu.getUkuranSepatu());
+            System.out.println("Warna Sepatu: " + sepatu.getWarnaSepatu());
+            System.out.println("Harga Sepatu: " + sepatu.getHargaSepatu());
+            System.out.println("Jumlah Sepatu: " + sepatu.getJumlahSepatu());
+        }
     
+}
 }
--- Sepatu.java
+++ Sepatu.java
@@ -1,59 +1,52 @@
 public class Sepatu {
-    private String id;
-    private String namaSepatu;
-    private String ukuranSepatu;
-    private String warnaSepatu;
-    private String hargaSepatu;
-    private String jumlahSepatu;
+    private String dataNamaSepatu;
+    private String dataUkuranSepatu;
+    private String dataWarnaSepatu;
+    private String dataHargaSepatu;
+    private String dataJumlahSepatu;
 
     //constructor untuk menginisialisasi objek dari sebuah kelas
-    public Sepatu(String id, String namaSepatu, String ukuranSepatu, String warnaSepatu, String hargaSepatu, String jumlahSepatu) {
-        this.id = id;
-        this.namaSepatu = namaSepatu;
-        this.ukuranSepatu = ukuranSepatu;
-        this.warnaSepatu = warnaSepatu;
-        this.hargaSepatu = hargaSepatu;
-        this.jumlahSepatu = jumlahSepatu;
+    public Sepatu(String dataNamaSepatu, String dataUkuranSepatu, String dataWarnaSepatu, String dataHargaSepatu, String dataJumlahSepatu) {
+        this.dataNamaSepatu = dataNamaSepatu;
+        this.dataUkuranSepatu = dataUkuranSepatu;
+        this.dataWarnaSepatu = dataWarnaSepatu;
+        this.dataHargaSepatu = dataHargaSepatu;
+        this.dataJumlahSepatu = dataJumlahSepatu;
     }
     
     //setter berguna untuk set value dari variabel itu semua
-    public void setId(String id) {
-        this.id = id;
+    public void setNamaSepatu(String dataNamaSepatu) {
+        this.dataNamaSepatu = dataNamaSepatu;
     }
-    public void setNamaSepatu(String namaSepatu) {
-        this.namaSepatu = namaSepatu;
+    public void setUkuranSepatu(String dataUkuranSepatu) {
+        this.dataUkuranSepatu = dataUkuranSepatu;
     }
-    public void setUkuranSepatu(String ukuranSepatu) {
-        this.ukuranSepatu = ukuranSepatu;
+    public void setWarnaSepatu(String dataWarnaSepatu) {
+        this.dataWarnaSepatu = dataWarnaSepatu;
     }
-    public void setWarnaSepatu(String warnaSepatu) {
-        this.warnaSepatu = warnaSepatu;
+    public void setHargaSepatu(String dataHargaSepatu) {
+        this.dataHargaSepatu = dataHargaSepatu;
     }
-    public void setHargaSepatu(String hargaSepatu) {
-        this.hargaSepatu = hargaSepatu;
-    }
-    public void setJumlahSepatu(String jumlahSepatu) {
-        this.jumlahSepatu = jumlahSepatu;
+    public void setJumlahSepatu(String dataJumlahSepatu) {
+        this.dataJumlahSepatu = dataJumlahSepatu;
     }
 
     //getter berfungsi untuk memanggil value dari variabel
-    public String getid() {
-        return id;
-    }
+
     public String getNamaSepatu() {
-        return namaSepatu;
+        return dataNamaSepatu;
     }
     public String getUkuranSepatu() {
-        return ukuranSepatu;
+        return dataUkuranSepatu;
     }
     public String getWarnaSepatu() {
-        return warnaSepatu;
+        return dataWarnaSepatu;
     }
     public String getHargaSepatu() {
-        return hargaSepatu;
+        return dataHargaSepatu;
     }
     public String getJumlahSepatu() {
-        return jumlahSepatu;
+        return dataJumlahSepatu;
     }
 
 }
